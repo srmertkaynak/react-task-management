7477b5db363928cdbb4df5a07e705f80c583660b
mertkaynak <kaynak.mert35@gmail.com>
2023-11-18 22:37:36 +0300

    Update

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TaskShow from "./TaskShow";
 
-const TaskList = () => {
-  return <div>TaskList</div>;
+const TaskList = ({ tasks }) => {
+  return (
+    <div>
+      {tasks.map((task, index) => {
+        return <TaskShow task={task} key={index} />;
+      })}
+    </div>
+  );
 };
 
 export default TaskList;
---
+++ src/components/TaskShow.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const TaskShow = ({ task }) => {
+  console.log(task);
+  return <div>a</div>;
+};
+
+export default TaskShow;
