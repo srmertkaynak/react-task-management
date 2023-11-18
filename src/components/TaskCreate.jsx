bbf1930bc072825defcdcc60778e1bc570e0a05a
mertkaynak <kaynak.mert35@gmail.com>
2023-11-18 22:16:49 +0300

    Update

--- src/components/TaskCreate.jsx
+++ src/components/TaskCreate.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TaskCreate = () => {
+  const [title, setTitle] = useState("");
+  const [taskDesc, setTaskDesc] = useState("");
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDescChange = (e) => {
+    setTaskDesc(e.target.value);
+  };
+
   return (
     <div className="task-create">
       <h3>Lütfen Task Ekleyiniz!</h3>
       <form className="task-form">
         <label className="task-label">Başlık</label>
-        <input className="task-input" />
+        <input
+          value={title}
+          onChange={handleTitleChange}
+          className="task-input"
+        />
         <label className="task-label">Açıklama</label>
-        <textarea className="task-input" />
+        <textarea
+          value={taskDesc}
+          onChange={handleDescChange}
+          className="task-input"
+          rows={4}
+        />
         <button className="task-button">Ekle</button>
       </form>
     </div>
