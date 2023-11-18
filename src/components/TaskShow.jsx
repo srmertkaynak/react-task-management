cd9aa75ec5a084062bcbd3ae0105c2cd58700886
mertkaynak <kaynak.mert35@gmail.com>
2023-11-18 23:11:02 +0300

    Update

--- src/components/TaskShow.jsx
+++ src/components/TaskShow.jsx
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TaskShow = ({ task, onDelete }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
   const handleDeleteClick = () => {
     onDelete(task.id);
   };
 
-  const handleUpdate = () => {
-    return;
+  const handleUpdateClick = () => {
+    setShowEdit(!showEdit);
   };
+
   return (
     <div className="task-show">
-      <h3 className="task-title">Başlık</h3>
-      <p>{task.title}</p>
-      <h3 className="task-title">Açıklama</h3>
-      <p>{task.taskDesc}</p>
-      <div>
-        <button
-          onClick={handleDeleteClick}
-          value={task.id}
-          className="task-button-delete">
-          Sil
-        </button>
-        <button onClick={handleUpdate} className="task-button-update">
-          Güncelle
-        </button>
-      </div>
+      {showEdit ? (
+        <></>
+      ) : (
+        <div>
+          <h3 className="task-title">Başlık</h3>
+          <p>{task.title}</p>
+          <h3 className="task-title">Açıklama</h3>
+          <p>{task.taskDesc}</p>
+          <div>
+            <button
+              onClick={handleDeleteClick}
+              value={task.id}
+              className="task-button-delete">
+              Sil
+            </button>
+            <button onClick={handleUpdateClick} className="task-button-update">
+              Güncelle
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
