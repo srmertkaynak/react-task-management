192893cb2b36fcf66aaa021c3ba45aea16cfd002
mertkaynak <kaynak.mert35@gmail.com>
2023-11-18 22:09:21 +0300

    Update

--- src/components/TaskCreate.jsx
+++ src/components/TaskCreate.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const TaskCreate = () => {
   return (
-    <div>
+    <div className="task-create">
       <h3>Lütfen Task Ekleyiniz!</h3>
       <form className="task-form">
-        <label>Başlık</label>
-        <input />
-        <label>Açıklama</label>
-        <textarea />
-        <button>Ekle</button>
+        <label className="task-label">Başlık</label>
+        <input className="task-input" />
+        <label className="task-label">Açıklama</label>
+        <textarea className="task-input" />
+        <button className="task-button">Ekle</button>
       </form>
     </div>
   );
